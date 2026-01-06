18ecbd2813962ec0382435ccd614a1efdfb6c1c2
Artyr Davydov <zillo236@gmail.com>
2026-01-06 18:58:06 +0300

    feat(article-params): reset params to defaults

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,34 +1,60 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from 'react';
-// import { defaultArticleState } from '../../constants/articleProps';
+import { useState, useEffect } from 'react';
+import {
+	defaultArticleState,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
+import { Select } from 'src/ui/select';
 
-// type Props = {
-// 	articleState: typeof defaultArticleState;
-// 	onApply: (state: typeof defaultArticleState) => void;
-// };
+type Props = {
+	articleState: typeof defaultArticleState;
+	onApply: (state: typeof defaultArticleState) => void;
+};
 
-export const ArticleParamsForm = () =>
-	// { articleState, onApply }: Props
-	{
-		const [isOpen, setIsOpen] = useState(false);
+export const ArticleParamsForm = ({ articleState, onApply }: Props) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [draftState, setDraftState] = useState(articleState);
 
-		return (
-			<>
-				<ArrowButton
-					isOpen={isOpen}
-					onClick={() => setIsOpen((prev) => !prev)}
-				/>
-				<aside className={clsx(styles.container, isOpen && styles.open)}>
-					<form className={styles.form}>
-						<div className={styles.bottomContainer}>
-							<Button title='Сбросить' htmlType='reset' type='clear' />
-							<Button title='Применить' htmlType='submit' type='apply' />
-						</div>
-					</form>
-				</aside>
-			</>
-		);
-	};
+	useEffect(() => {
+		if (isOpen) setDraftState(articleState);
+	}, [isOpen, articleState]);
+
+	return (
+		<>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen((prev) => !prev)} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						onApply(draftState);
+					}}
+					onReset={(e) => {
+						e.preventDefault();
+						setDraftState(defaultArticleState);
+						onApply(defaultArticleState);
+					}}>
+					<Select
+						title='Шрифт'
+						selected={draftState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							setDraftState((prev) => ({
+								...prev,
+								fontFamilyOption: option,
+							}))
+						}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</>
+	);
+};
