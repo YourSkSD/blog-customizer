f1c20ed32a0a8d11f3e4e85b064bf58d4dde7bb1
Artyr Davydov <zillo236@gmail.com>
2026-01-06 18:34:40 +0300

    Feat(Common): Form open/close

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,34 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useState } from 'react';
+// import { defaultArticleState } from '../../constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
-	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
-		</>
-	);
-};
+// type Props = {
+// 	articleState: typeof defaultArticleState;
+// 	onApply: (state: typeof defaultArticleState) => void;
+// };
+
+export const ArticleParamsForm = () =>
+	// { articleState, onApply }: Props
+	{
+		const [isOpen, setIsOpen] = useState(false);
+
+		return (
+			<>
+				<ArrowButton
+					isOpen={isOpen}
+					onClick={() => setIsOpen((prev) => !prev)}
+				/>
+				<aside className={clsx(styles.container, isOpen && styles.open)}>
+					<form className={styles.form}>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			</>
+		);
+	};
